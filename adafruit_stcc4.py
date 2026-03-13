9d6853b444787c17c1953de083baf2a74335da75
Liz <liz@adafruit.com>
2026-03-13 16:17:09 -0400

    pre-commit

--- adafruit_stcc4.py
+++ adafruit_stcc4.py
@@ -89,8 +89,7 @@ class STCC4:
         pid = self.product_id
         if pid != _STCC4_PRODUCT_ID:
             raise RuntimeError(
-                f"Failed to find STCC4 - expected product ID {_STCC4_PRODUCT_ID}, "
-                f"got 0x{pid}"
+                f"Failed to find STCC4 - expected product ID {_STCC4_PRODUCT_ID}, " f"got 0x{pid}"
             )
 
         self._co2: int = 0
